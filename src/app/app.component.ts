39bec002b8f5b540004495b761e393bfb5c2a406
Khaled Saleh <saleh208@hotmail.com>
2020-11-29 22:17:07 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,28 @@ import { Component } from '@angular/core';
 import { Configurations, dataProviderConfgToken, DataProviderServiceToken, LocalDataProvider,
    LocalDataProviderConfig, PREDICATES } from '@ksaleh/dropdown-lookup';
 import {take} from 'rxjs/operators';
+import { HttpDataService } from 'src/services/http-data-serivce';
 
+
+const random = (n=10)=> Math.floor(Math.random() * n);
+const names = ["Peter","Alan" , "Ali","Joe","Jason","Mason","Jack","Barry","Franki","Hunter"];
+const days = [3,7,10,11,14,17,20,21,25,28];
+const monthes = [1,2,3,5,6,7,8,10,11,12];
+const years = [1970,1982,1978,1987,1990,1995,2000,1984,2002,1998];
+const jobs = ['Testing Engineer','Devops Engineer ','Frontend Developer','Backend Developer',"Fullstack Developer",'Aystem Architect','Project Manager']
 let data :any[] =[];
 (()=>{
-   for(let i =0;i<51;i++)
-      data.push({id:i,name:'khaled saleh',job:'software engineer',birthdate:'08/20/1965',project:'front end'});
-      for(let i =51;i<100;i++)
-      data.push({id:i,name:'waleed saleh',job:'software engineer',birthdate:'08/20/1965',project:'front end'});
-
+   for(let i =0;i<300;i++)  {
+      let name = `${names[random()]} ${names[random()]} ${names[random()]} ${names[random()]}`;
+      let date = `${monthes[random()]}/${days[random()]}/${years[random()]}`;
+      let job = `${jobs[random(7)]}`;
+      data.push({id:i,name:name,job:job,birthdate:date,project:`Project_#${i}`});
+   }  
 })();
 
 const localDataConfg:LocalDataProviderConfig = {
   data : data,
-  pageSize :7
+  pageSize :10
 }
 
 
@@ -23,9 +32,9 @@ const localDataConfg:LocalDataProviderConfig = {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers :[
-        {provide: dataProviderConfgToken ,useValue :localDataConfg },
-        {provide: DataProviderServiceToken ,useClass : LocalDataProvider},
-      //  {provide: DataProviderServiceToken ,useClass : HttpDataService},
+      //{provide: dataProviderConfgToken ,useValue :localDataConfg },
+      //{provide: DataProviderServiceToken ,useClass : LocalDataProvider},
+       {provide: DataProviderServiceToken ,useClass : HttpDataService},
       ]
     
 })
@@ -35,7 +44,7 @@ export class AppComponent {
   constructor(){
   this.configurations = new Configurations();
   this.configurations.columns = 
-/*  
+ 
   [
     {field:'id',title:'ID',name:'ID',width:80},
     {field:'name',title:'Name',name:'Name',width:200},
@@ -43,8 +52,8 @@ export class AppComponent {
     {field:'gender',title:'Gender',name:'gender',width:80},
     {field:'status',title:'Status',name:'status',width:100},
   ] ;
-*/
-      
+
+/*      
   [
       {field:'id',title:'ID',name:'ID',width:80},
       {field:'name',title:'Name',name:'Name',width:170},
@@ -52,11 +61,14 @@ export class AppComponent {
       {field:'birthdate',title:'Date Of Birth',name:'Birth',width:100},
       {field:'project',title:'Project',name:'Project',width:75}
     ] ;
-  
-    this.configurations.filter = {field:'id',predicate:PREDICATES.GTE,value:null};
+*/
+    this.configurations.filter = {field:'name',predicate:PREDICATES.CONTAINS,value:null,ignoreCase:true};
     this.configurations.lookupField=()=> 'name';
     this.configurations.onSelect = (obs)=>{
-      obs.pipe(take(1)).subscribe(data=> console.log(data));
+      obs.pipe(take(1)).subscribe(data=> { 
+        console.log(data);
+        alert(`User selected : ${data.id},${data.name},${data.birthdate},${data.job}`);
+      });
     }
  
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import {DropdownLookupModule} from '@ksaleh/dropdown-lookup';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import "@angular/compiler";
 @NgModule({
   declarations: [
     AppComponent
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     DropdownLookupModule
   ],
   providers: [],
